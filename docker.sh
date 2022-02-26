d261e1b7962ee73b99e1e4797219cae802bc4635
4saito5 <4saito5@gmail.com>
2022-02-26 22:14:59 +0900

    Update docker.sh
    adding options for Lima

--- docker.sh
+++ docker.sh
@@ -17,17 +17,26 @@ abort() { echo "$*" >&2; exit 1; }
 unknown() { abort "unrecognized option '$1'"; }
 required() { [ $# -gt 1 ] || abort "option '$1' requires an argument"; }
 
+IS_LIMA=`which lima`
+if [[ "$IS_LIMA" =~ "not found" ]]; then
+  # echo "lima not found"
+  DOCKER_COMPOSE="docker-compose"
+else
+  # echo "lima found"
+  DOCKER_COMPOSE="lima nerdctl compose"
+fi
+
 ARG_ENV=''
 
 up() {
     echo "up ..."
     # docker-compose -f ./prov/docker/docker-compose.yml up -d --build
-    docker-compose -f ./prov/docker/docker-compose.yml up -d
+    ${DOCKER_COMPOSE} -f ./prov/docker/docker-compose.yml up -d
 }
 
 down() {
     echo "down ..."
-    docker-compose -f ./prov/docker/docker-compose.yml down
+    ${DOCKER_COMPOSE} -f ./prov/docker/docker-compose.yml down
 }
 
 while [ $# -gt 0 ]; do
